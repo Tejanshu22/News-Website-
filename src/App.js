34ec7f0bb43c976fb8e04ceb351d6bd7983135e3
Tejanshu <destejanshu@gmail.com>
2022-08-02 11:39:35 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import './App.css';
 
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
-import About from './Components/About';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
+      <Router>
+        <Fragment>
         <Navbar />
-        <News />
-        <About/>
+        <Routes>
+          <Route exact path="/" element={<News key="general" country='in' category='general'/>}/>
+          <Route exact path="/home" element={<News key="general" country='in' category='general'/>}/>
+          <Route exact path="/entertainment" element={<News key="entertainment" country='in' category='entertainment'/>}/>
+          <Route exact path="/health" element={<News key="health" country='in' category='health'/>}/>
+          <Route exact path="/sports" element={<News key="sports" country='in' category='sports'/>}/>
+          <Route exact path="/science" element={<News key="science" country='in' category='science'/>}/>
+          <Route exact path="/business" element={<News key="business" country='in' category='business'/>}/>
+          <Route exact path="/technology" element={<News key="technology" country='in' category='technology'/>}/>
+          <Route exact path="/general" element={<News key="general" country='in' category='general'/>}/>
+        </Routes>
+        </Fragment>
+      </Router>
       </div>
     )
   }
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
+
+    static defaultProps = {
+        country : 'in',
+        category : 'general'
+      }
+
+    static propTypes = {
+        country : PropTypes.string,
+        category: PropTypes.string
+    }
 
     constructor() {
         super();
@@ -14,7 +26,7 @@ export class News extends Component {
     }
 
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=736e36f1b8f84f97ab389028701b6bde&page=1&pageSize = 20";
+        let url = `https://newsapi.org/v2/top-headlines?country${this.props.country}&category=${this.props.category}&apiKey=736e36f1b8f84f97ab389028701b6bde&page=1&pageSize = 20`;
         this.setState({loading:true});
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -28,7 +40,7 @@ export class News extends Component {
     }
 
     handelPreviousClick = async () => {
-        let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=736e36f1b8f84f97ab389028701b6bde&page=${this.state.page - 1}&pageSize = 20`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=736e36f1b8f84f97ab389028701b6bde&page=${this.state.page - 1}&pageSize = 20`;
         this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -41,7 +53,7 @@ export class News extends Component {
     }
 
     handelNextClick = async () => {
-        let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=736e36f1b8f84f97ab389028701b6bde&page=${this.state.page + 1}&pageSize = 20`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=736e36f1b8f84f97ab389028701b6bde&page=${this.state.page - 1}&pageSize = 20`;
         if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
 
         }
